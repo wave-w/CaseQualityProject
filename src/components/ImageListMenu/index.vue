1d3d8eed71002f14614b91e41a07b395f86d5516
吴波 <wubo@wisonic.com>
2022-05-06 14:18:43 +0800

    添加图像菜单组件

---
+++ src/components/ImageListMenu/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="main">
+    <div
+      v-for="(url, index) in imageList"
+      :key="index"
+      class="image_box"
+      @click="$emit('selectImage', index)"
+    >
+      <img :src="url">
+      <div>{{ $t('caseList.ultrasonic_image') }}{{ index + 1 }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ImageListMenu',
+  props: {
+    imageList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: ['selectImage'],
+});
+</script>
+
+<style scoped lang='scss'>
+.main {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 750px !important;
+  overflow: hidden;
+  overflow-y: auto;
+  border: 1px solid #eee;
+  background: #000;
+  .image_box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    margin: 5px 0;
+    color: #fff;
+    cursor: pointer;
+    img {
+      width: 180px;
+      height: 180px;
+    }
+  }
+  .image_box:hover {
+    color: #e90f0f;
+  }
+}
+.main::-webkit-scrollbar { display: none; }
+</style>
